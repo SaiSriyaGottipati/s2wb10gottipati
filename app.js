63dc09b88698c64c227a838b972c8bb5769627e7
SaiSriyaGottipati <98078429+SaiSriyaGottipati@users.noreply.github.com>
2022-04-06 18:49:58 -0500

    adding files

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var VehiclesRouter = require('./routes/Vehicles');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Vehicles', VehiclesRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
